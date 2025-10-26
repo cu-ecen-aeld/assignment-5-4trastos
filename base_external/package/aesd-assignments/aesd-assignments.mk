1577bcc60cc02bf42b10806b431e07344321e0e1
4trastos <info@4trastos.com>
2025-10-26 13:15:47 +0100

    ok

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -1,15 +1,31 @@
-AESD_ASSIGNMENTS_VERSION = assignment-5-complete
-AESD_ASSIGNMENTS_SITE = https://github.com/cu-ecen-aeld/assignments-3-and-later-4trastos
+##############################################################
+#
+# AESD-ASSIGNMENTS Buildroot package
+#
+##############################################################
+
+AESD_ASSIGNMENTS_VERSION = main
+AESD_ASSIGNMENTS_SITE = 'git@github.com:cu-ecen-aeld/assignments-3-and-later-4trastos.git'
 AESD_ASSIGNMENTS_SITE_METHOD = git
 AESD_ASSIGNMENTS_GIT_SUBMODULES = YES
 
 define AESD_ASSIGNMENTS_BUILD_CMDS
-    $(MAKE) -C $(@D)/server $(TARGET_CONFIGURE_OPTS)
+    $(MAKE) -C $(@D)/finder-app CC="$(TARGET_CC)" CFLAGS="$(TARGET_CFLAGS)"
+    $(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)/server ARCH=$(BR2_ARCH) CROSS_COMPILE=$(TARGET_CROSS)
 endef
 
 define AESD_ASSIGNMENTS_INSTALL_TARGET_CMDS
+	$(INSTALL) -d 0755 $(@D)/conf/ $(TARGET_DIR)/etc/finder-app/conf/
+	$(INSTALL) -m 0755 $(@D)/conf/* $(TARGET_DIR)/etc/finder-app/conf/
+	$(INSTALL) -m 0755 $(@D)/assignment-autotest/test/assignment4/* $(TARGET_DIR)/bin
+
+	$(INSTALL) -m 0755 $(@D)/finder-app/finder.sh $(TARGET_DIR)/usr/bin
+	$(INSTALL) -m 0755 $(@D)/finder-app/finder-test.sh $(TARGET_DIR)/usr/bin
+	$(INSTALL) -m 0755 $(@D)/finder-app/writer $(TARGET_DIR)/usr/bin
+
     $(INSTALL) -D -m 0755 $(@D)/server/aesdsocket $(TARGET_DIR)/usr/bin/aesdsocket
-    $(INSTALL) -D -m 0755 $(@D)/server/aesdsocket-start-stop $(TARGET_DIR)/etc/init.d/S99aesdsocket
+	$(INSTALL) -D -m 0755 $(@D)/server/aesdsocket-start-stop $(TARGET_DIR)/etc/init.d/S99aesdsocket
+
 endef
 
 $(eval $(generic-package))
--- runqemu.sh
+++ runqemu.sh
@@ -9,16 +9,7 @@ qemu-system-aarch64 \
     -cpu cortex-a53 -nographic -smp 1 \
     -kernel buildroot/output/images/Image \
     -append "rootwait root=/dev/vda console=ttyAMA0" \
-    -netdev user,id=eth0,hostfwd=tcp::10022-:22 \
+    -netdev user,id=eth0,hostfwd=tcp::10022-:22,hostfwd=tcp::9000-:9000 \
     -device virtio-net-device,netdev=eth0 \
     -drive file=buildroot/output/images/rootfs.ext4,if=none,format=raw,id=hd0 \
     -device virtio-blk-device,drive=hd0 -device virtio-rng-pci
-    net nic \
-    -net user,hostfwd=tcp::${HOST_PORT_SSH}-:${GUEST_PORT_SSH},hostfwd=tcp::${HOST_PORT_AESD}-:${GUEST_PORT_AESD} \
-    "$@"
-
-# AÑADIR ESTAS LÍNEAS PARA REENVÍO DE PUERTOS:
-HOST_PORT_SSH=10022
-GUEST_PORT_SSH=22
-HOST_PORT_AESD=9000
-GUEST_PORT_AESD=9000

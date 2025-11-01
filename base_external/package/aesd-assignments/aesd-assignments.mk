efc21e3e7a759631522639d0128a3b5094bfe2a1
4trastos <info@4trastos.com>
2025-11-02 00:27:38 +0100

    assignment-assignment-5-complete

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -1,31 +1,21 @@
-##############################################################
-#
-# AESD-ASSIGNMENTS Buildroot package
-#
-##############################################################
-
 AESD_ASSIGNMENTS_VERSION = main
 AESD_ASSIGNMENTS_SITE = 'git@github.com:cu-ecen-aeld/assignments-3-and-later-4trastos.git'
 AESD_ASSIGNMENTS_SITE_METHOD = git
 AESD_ASSIGNMENTS_GIT_SUBMODULES = YES
 
+AESD_ASSIGNMENTS_MODULE_SUBDIRS = server
+
+# Compilar aesdsocket usando cross-compiler
 define AESD_ASSIGNMENTS_BUILD_CMDS
-    $(MAKE) -C $(@D)/finder-app CC="$(TARGET_CC)" CFLAGS="$(TARGET_CFLAGS)"
-    $(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)/server ARCH=$(BR2_ARCH) CROSS_COMPILE=$(TARGET_CROSS)
+	$(TARGET_CC) $(TARGET_CFLAGS) -o $(@D)/server/aesdsocket $(@D)/server/aesdsocket.c
 endef
 
+# Instalar binarios compilados y scripts de inicio
 define AESD_ASSIGNMENTS_INSTALL_TARGET_CMDS
-	$(INSTALL) -d 0755 $(@D)/conf/ $(TARGET_DIR)/etc/finder-app/conf/
-	$(INSTALL) -m 0755 $(@D)/conf/* $(TARGET_DIR)/etc/finder-app/conf/
-	$(INSTALL) -m 0755 $(@D)/assignment-autotest/test/assignment4/* $(TARGET_DIR)/bin
-
-	$(INSTALL) -m 0755 $(@D)/finder-app/finder.sh $(TARGET_DIR)/usr/bin
-	$(INSTALL) -m 0755 $(@D)/finder-app/finder-test.sh $(TARGET_DIR)/usr/bin
-	$(INSTALL) -m 0755 $(@D)/finder-app/writer $(TARGET_DIR)/usr/bin
-
-    $(INSTALL) -D -m 0755 $(@D)/server/aesdsocket $(TARGET_DIR)/usr/bin/aesdsocket
+	$(INSTALL) -D -m 0755 $(@D)/server/aesdsocket $(TARGET_DIR)/usr/bin/aesdsocket
 	$(INSTALL) -D -m 0755 $(@D)/server/aesdsocket-start-stop $(TARGET_DIR)/etc/init.d/S99aesdsocket
-
+	$(INSTALL) -D -m 0755 $(@D)/server/generate-ssh-keys $(TARGET_DIR)/etc/init.d/S49generate-ssh-keys
+	$(INSTALL) -d -m 0755 $(TARGET_DIR)/var/tmp
 endef
 
 $(eval $(generic-package))
